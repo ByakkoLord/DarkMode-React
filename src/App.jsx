ca40cad5f61b9162081a98529cede5d3656cbb5a
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-08-28 18:39:38 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen gap-4">
+      <p className="font-bold text-6xl dark:text-white">Hello World</p>
+      <button></button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useTheme.jsx
@@ -0,0 +1,23 @@
+import {useContext, createContext, useState, useEffect} from "react"
+
+const ThemeContext = createContext()
+
+export default function ThemeContextProvider({children}){
+    const [theme, setTheme] = useState("light")
+
+    useEffect(()=>{
+        const root = window.document.documentElement
+
+        root.classList.add(theme)
+    })
+
+    return(
+        <ThemeContext.Provider value={{theme, setTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export function useTheme(){
+    return useContext(ThemeContext)
+}
