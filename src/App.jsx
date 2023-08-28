7f088498ed602b0a0b53f8a92b4591adc244a427
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-08-28 18:51:51 -0300

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { useTheme } from "./hooks/useTheme";
 
 function App() {
+  const { theme, setTheme } = useTheme()
   return (
     <div className="flex justify-center items-center h-screen gap-4">
       <p className="font-bold text-6xl dark:text-white">Hello World</p>
-      <button></button>
+      {theme === "light" ? <button onClick={()=>setTheme("dark")} className="w-7 h-7 bg-red-500"/> : <button onClick={()=>setTheme("light")} className="w-7 h-7 bg-yellow-500"/>}
+      
     </div>
   );
 }
