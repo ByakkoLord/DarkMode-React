d1475e6a5bd46670998f2df6dc45e87979c9ab19
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-08-28 19:03:06 -0300

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useTheme } from "./hooks/useTheme";
+import Header from "./components/Header"
 
 function App() {
-  const { theme, setTheme } = useTheme()
+  
   return (
     <div className="flex justify-center items-center h-screen gap-4">
-      <p className="font-bold text-6xl dark:text-white">Hello World</p>
-      {theme === "light" ? <button onClick={()=>setTheme("dark")} className="w-7 h-7 bg-red-500"/> : <button onClick={()=>setTheme("light")} className="w-7 h-7 bg-yellow-500"/>}
+      
+      <Header/>
       
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { useTheme } from "../hooks/useTheme"
+
+function Header(){
+    const { theme, setTheme } = useTheme()
+    return(
+        <>
+        <p className="font-bold text-6xl dark:text-white">Hello World</p>
+      {theme === "light" ? <button onClick={()=>setTheme("dark")} className="w-7 h-7 bg-red-500"/> : <button onClick={()=>setTheme("light")} className="w-7 h-7 bg-yellow-500"/>}
+        </>
+    )
+}
+
+export default Header
