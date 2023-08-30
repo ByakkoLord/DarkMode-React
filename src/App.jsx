46a97ec57ce31052ff8a4eba6858fe73f54642b0
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-08-30 16:19:46 -0300

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import Header from "./components/Header"
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Zoom from 'react-reveal/Zoom';
 
-function App() {
-  
-  return (
-    <div className="flex justify-center items-center h-screen gap-4">
-      
-      <Header/>
-      
-    </div>
-  );
+class App extends Component {
+
+  handleButtonClick = (event) => {
+    const clickedComponentClass = event.target.className;
+
+    if (clickedComponentClass.includes("projectBox1")) {
+      this.setState({ textProject: "Project Box 1" });
+    } else if (clickedComponentClass.includes("projectBox2")) {
+      this.setState({ textProject: "Project Box 2" });
+    }
+  };
+
+  render() {
+    return (
+      <Zoom>
+        <div className="flex justify-center items-center h-screen gap-4">
+          <Header />
+        </div>
+      </Zoom>
+    );
+  }
 }
 
 export default App;
